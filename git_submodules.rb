41776cfdd8b894cd488e3d9963c85c134e793cfd
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-01-10 07:20:18 +0300

    Tas2Space

--- git_submodules.rb
+++ git_submodules.rb
@@ -1,72 +1,72 @@
 SUBS = %w[
-	USER_ROOM
-	USER_ROOM
-	git@github.com:the-cocoon/USER_ROOM.git
-	master
+  USER_ROOM
+  USER_ROOM
+  git@github.com:the-cocoon/USER_ROOM.git
+  master
 
-	RAILS_BLOG
-	RAILS_BLOG
-	git@github.com:the-cocoon/RAILS_BLOG.git
-	master
+  RAILS_BLOG
+  RAILS_BLOG
+  git@github.com:the-cocoon/RAILS_BLOG.git
+  master
 
-	RAILS_SHOP
-	RAILS_SHOP
-	git@github.com:the-cocoon/RAILS_SHOP.git
-	master
+  RAILS_SHOP
+  RAILS_SHOP
+  git@github.com:the-cocoon/RAILS_SHOP.git
+  master
 ].each_slice(4)
 
 def submodule_add(name, path, repo, branch)
-	puts cmd = "git submodule add -b #{branch} #{repo} #{ path }"
-	puts `#{ cmd }`
+  puts cmd = "git submodule add -b #{branch} #{repo} #{ path }"
+  puts `#{ cmd }`
 end
 
 def submodule_rm(name, path, repo, branch)
-	puts cmd = "git submodule deinit #{ name }"
-	puts `#{ cmd }`
+  puts cmd = "git submodule deinit #{ name }"
+  puts `#{ cmd }`
 
-	puts cmd = "git rm --cached #{ path }"
-	puts `#{ cmd }`
+  puts cmd = "git rm --cached #{ path }"
+  puts `#{ cmd }`
 
-	puts cmd = "rm -rf /modules/#{ path }"
-	puts `#{ cmd }`
+  puts cmd = "rm -rf /modules/#{ path }"
+  puts `#{ cmd }`
 
-	puts cmd = "rm -rf #{ path }"
-	puts `#{ cmd }`
+  puts cmd = "rm -rf #{ path }"
+  puts `#{ cmd }`
 end
 
 def submodules_init
-	puts cmd = "git submodule init"
-	puts `#{ cmd }`
+  puts cmd = "git submodule init"
+  puts `#{ cmd }`
 end
 
 def submodules_update
-	puts cmd = "git submodule update"
-	puts `#{ cmd }`
+  puts cmd = "git submodule update"
+  puts `#{ cmd }`
 end
 
 def add_submodules
-	SUBS.each do |sub|
-		name   = sub[0]
-		path   = sub[1]
-		repo   = sub[2]
-		branch = sub[3]
+  SUBS.each do |sub|
+    name   = sub[0]
+    path   = sub[1]
+    repo   = sub[2]
+    branch = sub[3]
 
-		submodule_add(name, path, repo, branch)
-	end
+    submodule_add(name, path, repo, branch)
+  end
 end
 
 def rm_submodules
-	SUBS.each do |sub|
-		name   = sub[0]
-		path   = sub[1]
-		repo   = sub[2]
-		branch = sub[3]
+  SUBS.each do |sub|
+    name   = sub[0]
+    path   = sub[1]
+    repo   = sub[2]
+    branch = sub[3]
 
-		submodule_rm(name, path, repo, branch)
-	end
+    submodule_rm(name, path, repo, branch)
+  end
 
-	puts cmd = "git reset --hard"
-	puts `#{ cmd }`
+  puts cmd = "git reset --hard"
+  puts `#{ cmd }`
 end
 
 add_submodules
